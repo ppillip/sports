7c781d137a3b72f89f533b401d25756c1c16c7e1
jungwon jin <jwjin@me.com>
2014-03-29 04:38:13 -0700

    fixtures.js getSchema로 변경

--- client/admin/router.js
+++ client/admin/router.js
@@ -6,14 +6,14 @@ Router.configure({
 Router.map(function () {
     this.route('admin', {
         path: '/admin',
-        action : function(){
+        action: function () {
             Route.go('dashBoard');
         }
     });
     this.route('dashBoard', {
         path: '/dashBoard',
-        template:'dashBoard',
-        data: function() {
+        template: 'dashBoard',
+        data: function () {
             console.log("dashBoard");
             return {
                 hey: "헤이 맨"
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,9 +1,6 @@
 if(CollectionSettings.find().count() === 0) {
     var obj = sportsSchema.getSchema('schema_settings');
     obj.title = 'Sports';
-    obj.point_sign_up = 'test';
 
-    CollectionSettings.insert({
-
-    });
+    CollectionSettings.insert(obj);
 }
