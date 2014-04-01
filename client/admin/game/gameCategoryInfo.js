d2516d31d3e5d0a7061b8fca4046e49d33316fb6
ppillip <ppillip@gmail.com>
2014-04-01 22:17:26 +0900

    for 부트스트

--- client/admin/game/gameCategoryInfo.js
+++ client/admin/game/gameCategoryInfo.js
@@ -5,8 +5,25 @@ Template.gameCategoryInfo.events({
 
 Template.gameCategoryInfo.rendered = function(){
 
+    console.log(this);
 };
 
 Template.gameCategoryInfo.helpers({
 
 });
+
+
+Template.gameInputForm.events({
+    'click input[name=save]' : function(e,tmpl){
+
+    }
+});
+
+Template.gameInputForm.rendered = function(){
+
+};
+
+Template.gameInputForm.helpers({
+
+});
+
