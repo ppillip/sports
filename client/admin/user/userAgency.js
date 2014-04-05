b64b6ec657a653bb03f230b88e788e382d6315d4
jungwon jin <jwjin@me.com>
2014-04-05 10:49:29 -0700

    질문위한 임시 커밋

--- client/admin/user/userAgency.js
+++ client/admin/user/userAgency.js
@@ -1,59 +1,85 @@
 
 Template.userAgency.events({
-    'click [name=createAgency]' : function(e,tmpl) {
-        e.preventDefault();
+	'click [name=createAgency]' : function(e,tmpl) {
+		e.preventDefault();
+		var schAgency = sportsSchema.getSchema('총판매장');
 
-        var schAgency = sportsSchema.getSchema('총판매장');
+		console.log('******');
+		console.log($(tmpl.find("select[name=소속_id]")).find('option:selected').attr('data-id'));
+//		$( tmpl.find('select[name=소속_id]')  ).find("option:selected").attr("data-id")
+//		console.log('###test###');
+//		console.log(e.currentTarget);
+//		console.log(tmpl);
+//		console.log($(tmpl.find("[name=소속_id]")));
+//		console.log(this.data._id);
 
-        _.each(schAgency,function(val,idx){
-            콘솔(tmpl.find("[name="+idx+"]"));
-//            콘솔($(tmpl.find("[name="+idx+"]")).val());
-            schAgency[idx] = $(tmpl.find("[name="+idx+"]")).val();
-        });
+		_.each(schAgency,function(val,idx){
+			//인풋값이 있는 놈만 넣어준다. 스킴구조 유지위함.
+			if(false) {
 
-        총판매장.insert(schAgency);
+			}
+			else if($(tmpl.find("[name="+idx+"]")).val()) {
+				schAgency[idx] = $(tmpl.find("[name="+idx+"]")).val();
+			}
+		});
 
-        tmpl.find('form').reset();
-        $(tmpl.find('[name=cancelCreateAgency]')).trigger('click');
+		console.log('### 저장된 객체 : ', schAgency);
+		console.log("### 저장된 총판매장 _id : ", 총판매장.insert(schAgency));
+
+		tmpl.find('form').reset();
+		$(tmpl.find('[name=cancelCreateAgency]')).trigger('click');
 
 //        $(tmpl.find('#myModal')).modal({'show':false});
 
-    },
-    'click #btn_remove' : function(e,tmp) {
-        e.preventDefault();
+	},
+	'click #btn_remove' : function(e,tmp) {
+		e.preventDefault();
 
-        총판매장.remove({_id:this._id});
-    }
+		총판매장.remove({_id:this._id});
+	}
 });
 
 Template.userAgency.rendered = function(){
 };
 
 Template.userAgency.helpers({
-    화면이름 : function() {
-        return this.화면이름;
-    }
+	화면이름 : function() {
+		return this.화면이름;
+	}
 });
+
+
 
 
 Template.agencyTable.helpers({
-    회원정보 : function() {
-        콘솔(this.화면이름);
-        if(this.화면이름 === '총판')
-            return 총판매장.find({$where:"this.소속_id.length === 0"});
-        else if(this.화면이름 === '매장')
-            return 총판매장.find({$where:"this.소속_id.length > 0"});
-        else
-            alert('Template.agencyTable.helpers({ : 화면이름에러');
-    }
-    ,
-    화면이름 : function() {
-        return this.화면이름;
-    }
+	회원정보 : function() {
+		콘솔(this.화면이름);
+		if(this.화면이름 === '총판')
+			return 총판매장.find({$where:"this.소속_id.length === 0"});
+		else if(this.화면이름 === '매장')
+			return 총판매장.find({$where:"this.소속_id.length > 0"});
+		else
+			alert('Template.agencyTable.helpers({ : 화면이름에러');
+	}
+	,화면이름 : function() {
+		return this.화면이름;
+	}
+
+
 });
 
+
+
+
 Template.userAgencyInputModal.helpers({
-    화면이름 : function() {
-        return this.화면이름;
-    }
+	화면이름 : function() {
+		return this.화면이름;
+	}
+	,총판리스트 : function() {
+		if(this.화면이름 === '매장') {
+			var 임시총판 = 총판매장.find({$where:"this.소속_id.length === 0"});
+
+			return 임시총판 = 총판매장.find({$where:"this.소속_id.length === 0"});
+		}
+	}
 });
