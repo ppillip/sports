aca8c008a6b99c7e974ce6d7fb4ad027929ce990
ppillip <ppillip@gmail.com>
2014-04-08 15:01:08 +0900

    popUP 공통

--- client/helper.js
+++ client/helper.js
@@ -3,9 +3,5 @@ Handlebars.registerHelper('isPopup', function (obj) {
 });
 
 Handlebars.registerHelper('현재화면이름', function (obj) {
-	cl("log");
-	cl(obj);
-	cl(Session.get("현재화면이름"));
-	cl(Session.get("현재화면이름") === obj);
-	return (obj === null) ? Session.get("현재화면이름") : Session.get("현재화면이름") === obj;
+	return (typeof obj === "undefined") ? Session.get("현재화면이름") : Session.get("현재화면이름") === obj;
 });
