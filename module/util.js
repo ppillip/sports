704ee6a8725dc3e20fc84e25a533379b6514e781
jungwon jin <jwjin@me.com>
2014-04-13 11:32:57 -0700

    아마존 몽고 디비 static ip 할당

--- module/util.js
+++ module/util.js
@@ -3,6 +3,6 @@ cl = function(a){Meteor.call('cl',a);}
 
 Meteor.methods({'cl' : function(msg){ console.log(msg);}});
 
-ClipStream = new Meteor.Stream('clip');
-//clip 오류로 인한 재 커밋 위한 주석
+//ClipStream = new Meteor.Stream('clip');
+
 
